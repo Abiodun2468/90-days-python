29087c399579cd51b417e1c609ea8c92f3e9545a
Abiodun2468 <136115768+Abiodun2468@users.noreply.github.com>
2025-01-07 10:08:32 -0800

    Update day_two.py

--- day_two.py
+++ day_two.py
@@ -1,1 +1,11 @@
+# Project :
+# create a program that takes users input for their name and age, then prints a greeting with their name and calculates the year they were born.
 
+name = input("enter your name: ")  # string input
+age = int(input("Enter your age : "))
+
+# calculating the year of birth
+current_year = 2025
+birth_year = 2025 - age
+
+print(f"Hello {name}, you were born in the year {year_of_birth}.")
